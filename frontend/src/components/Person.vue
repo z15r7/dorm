902a95083337ed728be9850b071c513b399ed41d
wendy0705 <wendy9007054@gmail.com>
2023-05-13 23:26:13 +0800

    抬頭及search bar

--- frontend/src/components/Person.vue
+++ frontend/src/components/Person.vue
@@ -1,16 +1,19 @@
 <template>
-    <div class="border border-slate-50 shadow shadow-slate-300 bg-white p-4 h-52 w-48 rounded-md cursor-pointer flex flex-col">
+    <div class="border border-slate-50 shadow shadow-slate-300 bg-white p-4 h-60 w-48 rounded-md cursor-pointer flex flex-col">
         <div class="flex justify-between">
             <p class="font-extrabold text-lg">{{ person.name }}</p>
             <p class="font-extrabold text-right text-xs mt-auto">{{ person.department }}</p>
         </div>
         <p class="font-bold text-xs text-zinc-500">
-            <span class = "bg-stone-200 px-2 mr-2 " v-for="(habit, index) in person.habits.split(' ')" :key="index">{{ '#' + habit }}</span>
+            <span class = "bg-zinc-100 px-2 mr-2 " v-for="(habit, index) in person.habits.split(' ')" :key="index">{{ '#' + habit }}</span>
             <p>{{ '最遠離學校' + person.distance + '分鐘' }}</p>
         </p>
-        <button class="text-xs text-right font-bold mt-auto inline-block mb-0">
-            邀請
-        </button>
+        <div class="flex justify-between mt-auto">
+            <p class = "text-xs text-left text-zinc-500 ">{{'已加入' + person.team +'個隊伍'}}</p>
+            <button class="border-2 rounded border-zinc-500 text-xs text-right font-bold inline-block ">
+                邀請
+            </button>
+        </div>
     </div>
 </template>
 
